2136ce11d3fabf2c9aec7e01d6ad0cb6feb8c297
boyu8266 <boryuh8266@gmail.com>
2023-07-17 20:33:42 +0800

    test: add MonthlyStock class and test_columns method to test_period.py
    - Add `MonthlyStock` class to `test_period.py`
    - Add `test_columns` method to `TestMonthlyStock` class
    - Add assertions for `Open`, `Low`, `High`, and `Close` values in `test_columns` method
    
    Signed-off-by: boyu8266 <boryuh8266@gmail.com>

--- tests/step/test_period.py
+++ tests/step/test_period.py
@@ -1,5 +1,5 @@
 from ist.state import IstState
-from ist.step import FetchData, WeeklyStock
+from ist.step import FetchData, MonthlyStock, WeeklyStock
 from tests.sleep import sleep
 
 
@@ -18,5 +18,24 @@ class TestWeeklyStock:
         assert abs(state.dataframe['Low'][0] - 565) < 0.01
         assert abs(state.dataframe['High'][0] - 591) < 0.01
         assert abs(state.dataframe['Close'][0] - 591) < 0.01
+
+        sleep()
+
+
+class TestMonthlyStock:
+    step = MonthlyStock('monthly stock')
+
+    def test_columns(self):
+        state = IstState()
+        state.stock = '2330'
+        state.start_date = '2023-6-1'
+        state.end_date = '2023-6-30'
+        FetchData('fetch data').run(state)
+        self.step.run(state)
+
+        assert abs(state.dataframe['Open'][0] - 550) < 0.01
+        assert abs(state.dataframe['Low'][0] - 550) < 0.01
+        assert abs(state.dataframe['High'][0] - 594) < 0.01
+        assert abs(state.dataframe['Close'][0] - 576) < 0.01
 
         sleep()
