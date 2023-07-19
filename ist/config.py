a0083bd536c71e22dc09fa0529fef86cc9b317a6
boyu8266 <boryuh8266@gmail.com>
2023-07-19 19:31:52 +0800

    chore: update gitignore and config files, add typing to config.py
    - Remove `logs` from the `.gitignore` file
    - Add `custom` to the `.gitignore` file
    - Add `config.ini` to the `.gitignore` file
    - Add `ist/custom.py` to the `.gitignore` file
    - Add `**.egg-info` to the `.gitignore` file
    - Add `build` to the `.gitignore` file
    - Add `dist` to the `.gitignore` file
    - Add `userid` to the `config.example.ini` file
    - Add `numbers`, `strategies`, and `periods` to the `config.example.ini` file
    - Add `from typing import List` to the `ist/config.py` file
    - Add `numbers: List[str]`, `strategies: List[str]`, and `periods: List[str]` to the `ist/config.py` file
    - Add initialization of `numbers`, `strategies`, and `periods` in the `Config` class in the `ist/config.py` file
    - Add `telegram_userid`, `numbers`, `strategies`, and `periods` as arguments to the `Config` class initialization in the `ist/config.py` file
    
    Signed-off-by: boyu8266 <boryuh8266@gmail.com>

--- ist/config.py
+++ ist/config.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from typing import List
 
 from pydantic import BaseModel
 
@@ -6,13 +7,23 @@ from pydantic import BaseModel
 class Config(BaseModel):
     telegram_token: str
     telegram_userid: str
+    numbers: List[str]
+    strategies: List[str]
+    periods: List[str]
 
     @classmethod
     def from_config_file(cls):
         config = ConfigParser()
         config.read('config.ini')
 
+        numbers = [num.strip() for num in config.get('Strategy', 'numbers').split(',')]
+        strategies = [strategy.strip() for strategy in config.get('Strategy', 'strategies').split(',')]
+        periods = [period.strip() for period in config.get('Strategy', 'periods').split(',')]
+
         return cls(
             telegram_token=config.get('Telegram', 'token'),
-            telegram_userid=config.get('Telegram', 'userid')
+            telegram_userid=config.get('Telegram', 'userid'),
+            numbers=numbers,
+            strategies=strategies,
+            periods=periods
         )
